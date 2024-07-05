0f3a6fb86a58801db4ed38a1ed6786bf193aab53
AliKandiAk <ali.jarsomah12@gmail.com>
2024-07-05 17:13:39 +0300

    update the managment js and design

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -33,6 +33,8 @@
 
 	<!-- Dark Themes -->
 	<link rel="stylesheet" href="{{asset('assets/styles/style-black.min.css')}}">
+{{-- boot strap icons  --}}
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
 
         /* .navigation .menu .menu-icon:hover {
