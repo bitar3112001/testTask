1a15ae8bf7a2563494f4dd0367d9b38a8908431a
bitar3112001 <141562620+bitar3112001@users.noreply.github.com>
2024-07-31 12:49:11 +0300

    login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function logIn(Request $request){
+        $incoming= $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        if(auth()->attempt(['email'=>$incoming['email'],'password'=>$incoming['password']])){
+            $request->session()->regenerate();
+             return redirect('/home');
+
+        }else{
+            return "sorry";
+        }
+    }
+
+    public function register(Request $request){
+        $incomingFields = $request->validate([
+            'name'=>"required",
+            'email'=>"required",
+            'password'=>"required"
+        ]);
+        $incomingFields['password']=bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        return redirect('/');;
+    }
+
 }
